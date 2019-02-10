71c2628a585c2e9b0c8ade54a6b3782ecb3f53d4
Girish <girish4200ster@gmail.com>
2019-02-11 00:04:07 +0100

    Updates

---
+++ precache-manifest.deb94b982575867ea1e3bcc638720aa9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "abc468973c9891643aed",
+    "url": "/girishtunk/robohashbuddies/static/js/runtime~main.abc46897.js"
+  },
+  {
+    "revision": "662fe660e5657db8c725",
+    "url": "/girishtunk/robohashbuddies/static/js/main.662fe660.chunk.js"
+  },
+  {
+    "revision": "9b2a21af56d2f7233988",
+    "url": "/girishtunk/robohashbuddies/static/js/1.9b2a21af.chunk.js"
+  },
+  {
+    "revision": "662fe660e5657db8c725",
+    "url": "/girishtunk/robohashbuddies/static/css/main.6d043359.chunk.css"
+  },
+  {
+    "revision": "9b2a21af56d2f7233988",
+    "url": "/girishtunk/robohashbuddies/static/css/1.641d94cd.chunk.css"
+  },
+  {
+    "revision": "8f364eac3cb86fc6f74c08ea60b0c3db",
+    "url": "/girishtunk/robohashbuddies/index.html"
+  }
+];
